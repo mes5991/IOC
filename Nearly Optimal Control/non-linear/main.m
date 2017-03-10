af38cd11e9cd39fe265902339851b47d53f8f7d2
Renato Girardi Gasoto <re.gasoto@gmail.com>
2017-03-10 02:54:01 -0500

    Add Lyapunov function generation - Next step generate L

--- Nearly Optimal Control/non-linear/main.m
+++ Nearly Optimal Control/non-linear/main.m
@@ -6,7 +6,7 @@ mono = [x1; x2; x2^3; x1^3; x1^2*x2; x1*x2^2; x2^5; x1^5; x1^4*x2; ...
              x1^3*x2^2; x1^2*x2^3; x1*x2^4; x2^7; x1^7; x1^6*x2; x1^5*x2^2; ...
              x1^4*x2^3; x1^3*x2^4; x1^2*x2^5; x1*x2^6];
          
-uF = matlabFunction(-tanh(weights*mono));
+uF = matlabFunction(-(weights*mono));
 
 x = [0;1];
 
@@ -22,29 +22,31 @@ syms x1 x2
 %             x1^3*x2^2; x1^2*x2^3; x1*x2^4; x2^7; x1^7; x1^6*x2; x1^5*x2^2; ...
 %             x1^4*x2^3; x1^3*x2^4; x1^2*x2^5; x1*x2^6];
 variables = sym('a',[1,length(x)]);
-h = monomials(variables,[1:7]);
+h = monomials(variables,[1:17]);
 hf = matlabFunction(h);
 H = reshape(hf(X(:,1),X(:,2)),length(t),length(h))';
 
 
 %%
 lambda = 0.039
-f = @(eta)abs(sum((tanh(eta'*H) - u).^2)) + lambda*eta'*eta
+f = @(eta)abs(sum(((eta'*H) - u).^2)) + lambda*eta'*eta
 eta = lsqnonlin(f,zeros(size(h)))
 
 
 
 
-control_law = @(x1,x2)tanh(eta'*hf(x1,x2));
 
-t2 = [0:0.01:30];
+%%
+control_law = @(x1,x2)(eta'*hf(x1,x2));
+x = [-0,-1]
+t2 = [0:0.01:90];
 [Xcomputed,ucomputed] = generate_sample([x(1);x(2)],t2,@(x1,x2)control_law(x1,x2));
 
 close all
 figure;
 plot(t,u)
 hold on
-plot(t,tanh(eta'*H),'-.r')
+plot(t,(eta'*H),'-.r')
 plot(t2,ucomputed,'-.b')
 hold off
 legend('expert','fitted','computed')
@@ -54,3 +56,37 @@ hold on
 plot(t2,Xcomputed,'-.')
 hold off
 legend('x1 expert','x2 expert','x1 computed','x2 computed')
+
+%% Compute Lyapunov Control
+syms x1 x2 
+vars = [x1;x2];
+% Constructing the vector field dx/dt = f
+f = [x1 + x2 - x1*(x1^2 + x2^2);
+     -x1 + x2 - x2*(x1^2 + x2^2) + control_law(x1,x2)];
+
+% =============================================
+% First, initialize the sum of squares program
+prog = sosprogram(vars);
+
+% =============================================
+% The Lyapunov function V(x): 
+mons = monomials([x1,x2],[1:8])
+[prog,V] = sospolyvar(prog,mons,'wscoeff');
+
+% =============================================
+% Next, define SOSP constraints
+
+% Constraint 1 : V(x) >= 0
+prog = sosineq(prog,V);
+
+% Constraint 2: (dV/dx)'*(f(x) + g(x)*u) <= 0
+expr = -(diff(V,x1)*f(1)+diff(V,x2)*f(2));
+prog = sosineq(prog,expr);
+
+% =============================================
+% And call solver
+prog = sossolve(prog);
+
+% =============================================
+% Finally, get solution
+SOLV = sosgetsol(prog,V)
