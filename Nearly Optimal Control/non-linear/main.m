5f6fad62a340063892c1188cbdd9c1cc73cf9229
Renato Girardi Gasoto <re.gasoto@gmail.com>
2017-03-21 12:15:51 -0400

    making control function from lyapunov function

--- Nearly Optimal Control/non-linear/main.m
+++ Nearly Optimal Control/non-linear/main.m
@@ -10,7 +10,7 @@ uF = matlabFunction(-(weights*mono));
 
 x = [0;1];
 
-t = 0:0.01:30;
+t = 0:0.01:90;
 
 
 [X,u] = generate_sample([x(1);x(2)],t,@(x1,x2)uF(x1,x2));
@@ -22,15 +22,16 @@ syms x1 x2
 %             x1^3*x2^2; x1^2*x2^3; x1*x2^4; x2^7; x1^7; x1^6*x2; x1^5*x2^2; ...
 %             x1^4*x2^3; x1^3*x2^4; x1^2*x2^5; x1*x2^6];
 variables = sym('a',[1,length(x)]);
-h = monomials(variables,[1:17]);
+h = monomials(variables,[1:8]);
 hf = matlabFunction(h);
 H = reshape(hf(X(:,1),X(:,2)),length(t),length(h))';
 
 
 %%
 lambda = 0.039
-f = @(eta)abs(sum(((eta'*H) - u).^2)) + lambda*eta'*eta
-eta = lsqnonlin(f,zeros(size(h)))
+%lambda = 0.1
+Dynamics = @(eta)abs(sum(((eta'*H) - u).^2)) + lambda*eta'*eta
+eta = lsqnonlin(Dynamics,zeros(size(h)))
 
 
 
@@ -42,26 +43,12 @@ x = [-0,-1]
 t2 = [0:0.01:90];
 [Xcomputed,ucomputed] = generate_sample([x(1);x(2)],t2,@(x1,x2)control_law(x1,x2));
 
-close all
-figure;
-plot(t,u)
-hold on
-plot(t,(eta'*H),'-.r')
-plot(t2,ucomputed,'-.b')
-hold off
-legend('expert','fitted','computed')
-figure
-plot(t,X)
-hold on
-plot(t2,Xcomputed,'-.')
-hold off
-legend('x1 expert','x2 expert','x1 computed','x2 computed')
 
 %% Compute Lyapunov Control
-syms x1 x2 
+syms x1 x2 real
 vars = [x1;x2];
 % Constructing the vector field dx/dt = f
-f = [x1 + x2 - x1*(x1^2 + x2^2);
+Dynamics = [x1 + x2 - x1*(x1^2 + x2^2);
      -x1 + x2 - x2*(x1^2 + x2^2) + control_law(x1,x2)];
 
 % =============================================
@@ -80,7 +67,7 @@ mons = monomials([x1,x2],[1:8])
 prog = sosineq(prog,V);
 
 % Constraint 2: (dV/dx)'*(f(x) + g(x)*u) <= 0
-expr = -(diff(V,x1)*f(1)+diff(V,x2)*f(2));
+expr = -(gradient(V,[x1;x2])'*Dynamics+gradient(V,[x1;x2])'*Dynamics);
 prog = sosineq(prog,expr);
 
 % =============================================
@@ -89,4 +76,50 @@ prog = sossolve(prog);
 
 % =============================================
 % Finally, get solution
-SOLV = sosgetsol(prog,V)
+V = sosgetsol(prog,V)
+
+
+%% Formulate Control from CLF
+% Assume sigma(x) = sqrt((Vx*(f(x)))^2 + Vx*g(x)'*R*Vx*g(x))
+Vx = gradient(V,[x1;x2]);
+ f = [x1 + x2 - x1*(x1^2 + x2^2);
+     -x1 + x2 - x2*(x1^2 + x2^2)];
+ g = [0;1]
+phi = -(Vx'*f + sqrt((Vx'*f)^2 + (Vx'*g*control_law(x1,x2))^2))/(Vx'*g);
+phi_fun = matlabFunction(phi)
+
+%%
+x = [0;1];
+t2 = [0:0.01:90];
+[XcomputedPhi,ucomputedPhi] = generate_sample([x(1);x(2)],t2,@(x1,x2)phi_fun(x1,x2));
+close all
+figure;
+plot(t,u)
+hold on
+plot(t,(eta'*H),'-.r')
+plot(t2,ucomputed,'-.b')
+plot(t2,ucomputedPhi,'--')
+hold off
+legend('expert','fitted','estimated','computed from Lyapunov')
+figure
+plot(t,X)
+hold on
+plot(t2,Xcomputed,'-.')
+plot(t2,XcomputedPhi,'--')
+legend('x1 expert','x2 expert','x1 estimated','x2 estimated','x1 computed from Lyapunov','x2 computed from Lyapunov')
+
+%% compute sigma such that inf (Vx*(f(x)+g(x)*u) <= sigma(x)
+sigma = -Vx'*f - Vx'*g;
+l1 = (sigma - (Vx'*f))^2*(Vx'*g)^2
+l1_fun = matlabFunction(l1)
+
+l1Demonstrated = l1_fun(X(:,1),X(:,2))
+l1Estimated = l1_fun(Xcomputed(:,1),Xcomputed(:,2))
+l1ComputedPhi = l1_fun(XcomputedPhi(:,1),XcomputedPhi(:,2))
+%%
+figure
+semilogy(t,l1Demonstrated)
+hold on
+semilogy(t2,l1Estimated,'-.')
+semilogy(t2,l1ComputedPhi,'--')
+grid on
